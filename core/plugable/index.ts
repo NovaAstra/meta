07f5498f59f0971b86179d92ee234f382b46218c
NovaAstra <NovaAstra@outlook.com>
2024-08-18 18:51:39 +0800

    main

--- core/plugable/index.ts
+++ core/plugable/index.ts
@@ -1,5 +1,14 @@
-export type Plugin = {
-    name: string;
+export type Setup<Hooks, API> = (api: API) => void
+
+export type PluginOptions<Hooks, Setup = undefined> = {
+    name?: string;
+    pre?: string[];
+    post?: string[];
+    rivals?: string[];
+    required?: string[];
+    setup?: Setup;
+    usePlugins?: PluginOptions<any, any>[];
+    registerHook?: Partial<Hooks>;
 }
 
-export function checkPlugins() { }
+export type Plugin<Hooks, API> = Required<PluginOptions<Hooks, Setup<Hooks, API>>>
