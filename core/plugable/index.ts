eacf0b457355d90eecf11cd04f7a52fd81386648
AugustGao <August.Gao@mspbots.ai>
2024-08-19 18:19:36 +0800

    main

--- core/plugable/index.ts
+++ core/plugable/index.ts
@@ -1,3 +1,5 @@
+// @ts-nocheck
+
 export type Setup<Hooks, API> = (api: API) => void
 
 export type PluginOptions<Hooks, Setup = undefined> = {
@@ -11,4 +13,36 @@ export type PluginOptions<Hooks, Setup = undefined> = {
     registerHook?: Partial<Hooks>;
 }
 
+
 export type Plugin<Hooks, API> = Required<PluginOptions<Hooks, Setup<Hooks, API>>>
+
+
+export function createScheduler(hooks, models) {
+    const scheduler = Object.create()
+
+    if (models) {
+        for (const key in models) {
+
+        }
+
+        scheduler[key] = (...args) => models[key].run.apply(null, ...args)
+    }
+
+    return scheduler;
+}
+
+export class PluginFactory {
+    private plugins = []
+
+    public constructor(plugins) {
+        this.plugins = [...plugins]
+    }
+
+    public bootstrap() { }
+}
+
+export function plugable() {
+    const factory = new PluginFactory();
+    const scheduler = factory.bootstrap()
+    return scheduler
+}
