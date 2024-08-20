030272ebf03f90457fc570197779d47a112ea149
AugustGao <August.Gao@mspbots.ai>
2024-08-20 18:59:35 +0800

    main

--- core/hookable/index.ts
+++ core/hookable/index.ts
@@ -1,113 +1,80 @@
-// @ts-nocheck
-
-export type Next<T, O> = (input?: T) => O;
-
-export type Middleware<T, O> = (input: T, next: Next<T, O>) => O
-
-export type Middlewares<T, O> = Middleware<T, O>[];
-
-export type PipelineLike<T, O> = { middlewares: Middlewares<T, O> };
-
-export type MiddlewareInput<T, O> = Middleware<T, O> | Middlewares<T, O> | PipelineLike<T, O>;
-
-export function getMiddlewares<T, O>(input: MiddlewareInput<T, O>): Middlewares<T, O> {
-    if (typeof input === 'function') return [input];
-
-    if (Array.isArray(input)) return input
-
-    if (input.middlewares) return input.middlewares
-
-    throw new Error(`${input} is not a valid Middleware`);
-}
+import { Pipeline } from "@meta-core/pipeable"
 
 export function unshift<T>(element: T, args: T[]): T[] {
     return element ? [element].concat(args.slice(0, -1)) : args;
 }
 
-export class Pipeline<T = unknown, O = unknown> implements PipelineLike<T, O> {
-    public middlewares: Middlewares<T, O> = []
+export abstract class HookFactory<I, O> {
+    protected pipeline = new Pipeline<I, O>()
 
-    public use(middleware: MiddlewareInput<T, O>): void {
-        const middlewares = getMiddlewares<T, O>(middleware)
-
-        this.middlewares.push(...middlewares)
-    }
-
-    public start(input: T): O {
-        return this.dispatch(0, input);
-    }
-
-    private dispatch(index: number, input: T): O {
-        const fn = this.middlewares[index];
-        const next = ((nextInput = input) => this.dispatch(index + 1, nextInput)) as Next<T, O>
-
-        return fn(input, next);
+    public call(): O {
+        return this.pipeline.start(arguments as I)
     }
 }
 
-export abstract class HookFactory<T, O> extends Pipeline<T, O> {
-    public call(...args: any[]) {
-        this.start(args as T)
-    }
-}
-
-export class SyncHook<T, O> extends HookFactory<T, O> {
-
-}
-
-export class SyncBailHook<T, O> extends HookFactory<T, O> {
+export class SyncHook<I, O> extends HookFactory<I, O> {
     public tap(callback) {
-        this.use((input, next) => {
-            const result = callback(...input)
-
-            if (result !== undefined) return result;
+        this.pipeline.use((input, next) => {
+            callback(input)
 
             return next()
         })
     }
 }
 
-export class SyncWaterfallHook<T, O> extends HookFactory<T, O> {
-    public tap(callback) {
-        this.use((input, next) => {
-            const result = callback(...input)
+// export class SyncBailHook<T, O> extends HookFactory<T, O> {
+//     public tap(callback) {
+//         this.use((input, next) => {
+//             const result = callback(...input)
 
-            const nextInput = unshift(result, input)
+//             if (result !== undefined) return result;
 
-            return next(nextInput)
-        })
-    }
-}
+//             return next()
+//         })
+//     }
+// }
 
-export class SyncLoopHook<T, O> extends HookFactory<T, O> {
-    public tap(callback) {
+// export class SyncWaterfallHook<T, O> extends HookFactory<T, O> {
+//     public tap(callback) {
+//         this.use((input, next) => {
+//             const result = callback(...input)
 
-    }
-}
+//             const nextInput = unshift(result, input)
 
-export class AsyncParallelHook<T, O> extends HookFactory<T, O> {
-    public tap(callback) {
-        this.use((input, next) => {
-            callback(...input)
+//             return next(nextInput)
+//         })
+//     }
+// }
 
-            next()
-        })
-    }
-}
+// export class SyncLoopHook<T, O> extends HookFactory<T, O> {
+//     public tap(callback) {
 
-export class AsyncParallelBailHook<T, O> extends HookFactory<T, O> {
+//     }
+// }
 
-}
+// export class AsyncParallelHook<T, O> extends HookFactory<T, O> {
+//     public tap(callback) {
+//         this.use((input, next) => {
+//             callback(...input)
 
-export class AsyncSeriesHook<T, O> extends HookFactory<T, O> { }
+//             next()
+//         })
+//     }
+// }
 
-export class AsyncSeriesBailHook<T, O> extends HookFactory<T, O> {
-    public tap(callback) {
-        this.use((input, next) => {
-            callback(...input).then(result => result !== undefined ? result : next())
-        })
-    }
-}
+// export class AsyncParallelBailHook<T, O> extends HookFactory<T, O> {
 
-export class AsyncSeriesWaterfallHook<T, O> extends HookFactory<T, O> { }
+// }
+
+// export class AsyncSeriesHook<T, O> extends HookFactory<T, O> { }
+
+// export class AsyncSeriesBailHook<T, O> extends HookFactory<T, O> {
+//     public tap(callback) {
+//         this.use((input, next) => {
+//             callback(...input).then(result => result !== undefined ? result : next())
+//         })
+//     }
+// }
+
+// export class AsyncSeriesWaterfallHook<T, O> extends HookFactory<T, O> { }
 
