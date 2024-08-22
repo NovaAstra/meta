6192197f85175b84c509c8378542397dea59c644
NovaAstra <NovaAstra@outlook.com>
2024-08-22 21:58:43 +0800

    main

--- core/hookable/index.ts
+++ core/hookable/index.ts
@@ -1,4 +1,4 @@
-import { type Arguments } from "@meta-core/typeable"
+import { type Arguments,type Promisable } from "@meta-core/typeable"
 import { Pipeline } from "@meta-core/pipeable"
 
 export function unshift<R, I extends Arguments>(result: R, args: I): I {
@@ -12,7 +12,7 @@ export abstract class HookFactory<I extends Arguments, O> {
         return this.pipeline.start(args)
     }
 
-    public abstract tap(callback: (...args: I) => O): void
+    public abstract tap(callback: (...args: I) => Promisable<O>): void
 }
 
 export class SyncHook<I extends Arguments, O = void> extends HookFactory<I, O> {
@@ -54,22 +54,32 @@ export class SyncLoopHook<I extends Arguments, O = void> extends HookFactory<I, 
 }
 
 
+export class AsyncParallelHook<I extends Arguments, O = void> extends HookFactory<I, O> {
+    public tap(callback: (...args: I) => O): void {
+        this.pipeline.use((input, next) => {
+            callback(...input)
+            return next()
+        })
+    }
+}
 
-// export class AsyncParallelHook<T, O> extends HookFactory<T, O> {
-//     public tap(callback) {
-//         this.use((input, next) => {
-//             callback(...input)
+export class AsyncParallelBailHook<I extends Arguments, O = void> extends HookFactory<I, O> {
+    public tap(callback: (...args: I) => O): void {
+        this.pipeline.use((input, next) => {
+            callback(...input)
+            return next()
+        })
+    }
+}
 
-//             next()
-//         })
-//     }
-// }
-
-// export class AsyncParallelBailHook<T, O> extends HookFactory<T, O> {
-
-// }
-
-// export class AsyncSeriesHook<T, O> extends HookFactory<T, O> { }
+export class AsyncSeriesHook<I extends Arguments, O = void> extends HookFactory<I, O> {
+    public tap(callback: (...args: I) => Promise<O>): void {
+        this.pipeline.use((input, next) => {
+            callback(...input).then(() => next())
+            return next()
+        })
+    }
+}
 
 // export class AsyncSeriesBailHook<T, O> extends HookFactory<T, O> {
 //     public tap(callback) {
