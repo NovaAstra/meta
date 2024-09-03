eb959cfa913b4ceffda87004e7f749c045cb9b75
AugustGao <August.Gao@mspbots.ai>
2024-09-03 18:52:04 +0800

    main

--- vue/demo/src/core/useTableObserver.ts
+++ vue/demo/src/core/useTableObserver.ts
@@ -1,11 +1,17 @@
 import { useResizeObserver } from "./useResizeObserver"
 
 export function useTableObserver() {
+    let rootElement: HTMLElement | undefined
+
     const observer = useResizeObserver()
 
     return {
-        observeRoot() { },
+        observeRoot(element: HTMLElement) {
+            observer.observe(rootElement = element)
+        },
         observeCell() { },
-        dispose: observer.disconnect
+        dispose() {
+            observer.disconnect()
+        }
     }
 }
