75efcac5539b9f8ea27b267e973fe82615abf3fe
AugustGao <August.Gao@mspbots.ai>
2024-09-04 11:49:15 +0800

    cell

--- vue/demo/src/core/Cell.vue
+++ vue/demo/src/core/Cell.vue
@@ -1,5 +1,27 @@
 <script lang="ts" setup>
+import { CSSProperties, type IntrinsicElementAttributes, computed } from "vue"
 
+export type DOMElements = keyof IntrinsicElementAttributes
+
+export interface CellProps {
+    width: number;
+    height: number;
+    left: number;
+    top: number;
+    tag?: DOMElements,
+}
+
+const props = withDefaults(defineProps<CellProps>(), {
+    tag: 'div'
+})
+
+const style = computed<CSSProperties>(() => ({
+    position: 'absolute',
+    display: 'grid',
+    width: props.width
+}))
 </script>
 
-<template></template>
+<template>
+    <component :is="tag" :style="style"></component>
+</template>
