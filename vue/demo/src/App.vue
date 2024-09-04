43717682bc8af05a0515c4fd49a1185b8357a144
AugustGao <August.Gao@mspbots.ai>
2024-09-04 14:54:58 +0800

    cell

--- vue/demo/src/App.vue
+++ vue/demo/src/App.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
+import { useTableObserver } from "./core/useTableObserver"
+import Cell from "./core/Cell.vue"
 
+const observer = useTableObserver()
 </script>
 
 <template>
-
+    <div class="viewport">
+        <Cell :top="40" :left="100" :width="200" :height="80" :observer="observer"> demo</Cell>
+    </div>
 </template>
--- vue/demo/src/core/Cell.vue
+++ vue/demo/src/core/Cell.vue
@@ -1,8 +1,8 @@
 <script lang="ts" setup>
-import { CSSProperties, type IntrinsicElementAttributes, computed, watch } from "vue"
+import { CSSProperties, type NativeElements, computed, ref, watch } from "vue"
 import { type Observer } from "./useTableObserver"
 
-export type DOMElements = keyof IntrinsicElementAttributes
+export type DOMElements = keyof NativeElements
 
 export interface CellProps {
     width: number;
@@ -17,13 +17,30 @@ const props = withDefaults(defineProps<CellProps>(), {
     element: 'div'
 })
 
+const elRef = ref<HTMLElement>()
+
 const style = computed<CSSProperties>(() => ({
-    position: 'absolute',
     display: 'grid',
-    width: props.width
+    position: 'absolute',
+    width: props.width,
+    height: props.height,
+    top: props.top,
+    left: props.left
 }))
+
+watch(
+    () => elRef.value,
+    () => {
+        console.log(1)
+    },
+    {
+        flush: "post"
+    }
+)
 </script>
 
 <template>
-    <component :is="element" :style="style"></component>
+    <component ref="elRef" :is="element" :style="style">
+        <slot></slot>
+    </component>
 </template>
