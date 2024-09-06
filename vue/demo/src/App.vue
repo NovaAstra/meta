f4d469a1a341d79fdced980e97b327af985aa2bd
AugustGao <August.Gao@mspbots.ai>
2024-09-06 18:54:06 +0800

    main

--- vue/demo/src/App.vue
+++ vue/demo/src/App.vue
@@ -1,8 +1,40 @@
 <script setup lang="ts">
+import { ref, onMounted } from 'vue';
+
+const viewport = ref()
+
+onMounted(() => {
+   viewport.value.scrollTop = 400
+})
 </script>
 
 <template>
-    <table class="viewport">
-       
-    </table>
+   <div class="viewport" ref="viewport">
+      <table>
+         <tr v-for="ridx in 60" :key="ridx">
+            <td class="cell" v-for="cidx in 20" :key="cidx">{{ ridx * cidx }}</td>
+         </tr>
+      </table>
+   </div>
 </template>
+
+<style lang="css" scoped>
+.viewport {
+   width: 500px;
+   height: 500px;
+   overflow: auto;
+   border: 1px solid red;
+   position: relative;
+}
+
+table{
+   position: absolute;
+   top: 0;
+}
+
+.cell {
+   width: 100px;
+   height: 50px;
+   border: 1px solid blue;
+}
+</style>
--- vue/demo/src/core/store.ts
+++ vue/demo/src/core/store.ts
@@ -1,3 +1,12 @@
+// @ts-nocheck
+
+export enum ActionEnum {
+    ELEMENT_RESIZE,
+    VIEWPORT_RESIZE
+}
+
 export class Store {
-    public update() { }
+    public update(action: ActionEnum, payload) {
+
+    }
 }
