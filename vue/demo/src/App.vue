18213da81372a7d58885758fe6831d644e6bd85a
NovaAstra <NovaAstra@outlook.com>
2024-09-21 16:26:07 +0800

    main

--- vue/demo/src/App.vue
+++ vue/demo/src/App.vue
@@ -1,14 +1,8 @@
 <script lang="ts" setup>
-
 </script>
 
 <template>
    <div class="root">
-      <table border="1">
-         <tr v-for="ridx in 500" :key="ridx">
-            <td v-for="cidx in 30" :key="cidx">{{ 100 * (ridx - 1) + cidx }}</td>
-         </tr>
-      </table>
    </div>
 </template>
 
@@ -16,17 +10,5 @@
 .root {
    width: 500px;
    height: 500px;
-   border: 1px solid red;
-   overflow: auto;
-}
-
-table{
-   border-collapse: collapse;
-}
-
-td{
-   max-width: 20px !important;
-   border: 1px solid blue;
-   word-wrap: break-word;
 }
 </style>
--- vue/demo/src/core/dom.ts
+++
@@ -1,5 +0,0 @@
-export const getCurrentDocument = (node: HTMLElement): Document =>
-    node.ownerDocument;
-
-
-export const getCurrentWindow = (doc: Document): (Window & typeof globalThis) => doc.defaultView!;
