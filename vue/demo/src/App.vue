b9d814da6aa64978dfc4f890f50fb112acec26c4
NovaAstra <NovaAstra@outlook.com>
2024-09-23 21:14:53 +0800

    main

--- vue/demo/src/App.vue
+++ vue/demo/src/App.vue
@@ -1,17 +1,8 @@
 <script lang="ts" setup>
-import { List } from "./List"
 </script>
 
 <template>
    <div class="root">
-      <List />
+    
    </div>
 </template>
-
-<style lang="css">
-.root {
-   width: 500px;
-   height: 500px;
-   position: relative;
-}
-</style>
--- vue/demo/src/core/model.ts
+++
@@ -1,7 +0,0 @@
-export class Model {
-    public constructor(
-        public length: number = 0,
-        public size: number = 50,
-        public autosizes: number[] = []
-    ) { }
-}
