a770ac76acfa1a3642b8d9cf7b15f9531f4d0ed5
AugustGao <August.Gao@mspbots.ai>
2024-10-18 18:20:37 +0800

    初始化

--- vue/demo/src/core.ts
+++ vue/demo/src/core.ts
@@ -1,22 +1,28 @@
-export type Snapshot = [sizes: number[], override: number[], size: number]
+export type Snapshot = [indices: number[], override: number[], size: number]
 
 export const DEFAULT_SIZE = 60
 
 export class ViewModel {
-    public size: number
+    public size: number = DEFAULT_SIZE
 
     public length: number = 0
 
-    public indices: number[] = []
+    public indices: Float32Array = new Float32Array()
+
+    public override: Float32Array = new Float32Array()
 
     public readonly snapshot?: Snapshot
 
     public constructor(
+        size: number,
         length: number,
-        size: number = DEFAULT_SIZE,
         snapshot?: Snapshot
     ) {
         this.length = length
+        this.snapshot = snapshot
+
         this.size = snapshot ? snapshot[2] : size
     }
+
+    public clone() { }
 }
