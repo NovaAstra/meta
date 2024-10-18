fdd9140d0086d9af1bf76a1294f17ff3a3df80cd
AugustGao <August.Gao@mspbots.ai>
2024-10-18 16:05:35 +0800

    初始化

--- vue/demo/src/App.vue
+++ vue/demo/src/App.vue
@@ -1,35 +1,7 @@
 <script lang="ts" setup>
-import { List } from "./List"
 </script>
 
 <template>
    <div class="root">
-      <List />
    </div>
 </template>
-
-<style>
-.root {
-   width: 500px;
-   height: 500px;
-   border: 1px solid red;
-   position: relative;
-}
-
-.list-root {
-   overflow: auto;
-   position: absolute;
-   width: 100%;
-   height: 100%;
-}
-
-.list-scroll-clip {
-   position: sticky;
-   contain: strict;
-   overflow-anchor: none;
-   width: 100%;
-   height: 100%;
-   top: 0px;
-   left: 0px;
-}
-</style>
