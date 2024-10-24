b88925c4043792e8fede6a107086c05a55ff3465
AugustGao <August.Gao@mspbots.ai>
2024-10-24 11:44:48 +0800

    初始化

--- core/codeable/nanoid.ts
+++
@@ -1,5 +0,0 @@
-import { customAlphabet } from 'nanoid'
-
-const alphabet = '23456789ABCDEFGHJKLMNPQRSTWXYZabcdefghijkmnopqrstuvwxyz';
-
-export const guid = (size: number = 12): (size?: number) => string => customAlphabet(alphabet, size)
